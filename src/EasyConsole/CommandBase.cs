ea010b9a94500ddd2f450a4ac93d8e0dab3cf388
Joe Kruger <joe.kruger@besttalk.co.uk>
2023-01-10 20:34:05 +0000

    Make disposable

--- src/EasyConsole/CommandBase.cs
+++ src/EasyConsole/CommandBase.cs
@@ -6,8 +6,12 @@ using System.Threading.Tasks;
 
 namespace EasyConsole
 {
-    public abstract class CommandBase
+    public abstract class CommandBase : IDisposable
     {
         public abstract Task ExecuteAsync(IServiceProvider provider);
+        public virtual void Dispose()
+        {
+            //nop
+        }
     }
 }
